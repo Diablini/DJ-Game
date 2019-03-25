7d863c928dc6dc503be02140259b5897181124eb
Alexander Branev <a.branev@mail.bg>
2019-03-25 12:09:41 +0200

    initial project base

---
+++ android/src/com/dj/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dj.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dj.game.DJGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DJGame(), config);
+	}
+}
